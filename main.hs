93b8f39a614b1b6897051dafbd7fd18f170003db
Anja Berens <pharaun666@gmail.com>
2013-06-29 22:28:59 -0700

    primitive hxt parsing and primitive http worker, need to tchan & conduit it

--- main.hs
+++ main.hs
@@ -1,9 +1,3 @@
-import Data.List
-import Control.Arrow.ArrowTree
-import Text.XML.HXT.Core
-
-
-
 {-
     1. Basic structure
         a. Fetch a comic page
@@ -35,19 +29,75 @@ import Text.XML.HXT.Core
                 - comic
                 - volume, chapter
 -}
+import Data.List
+import Control.Arrow.ArrowTree
+import Text.XML.HXT.Core
+
+import Network
+
+import Data.Conduit.Binary (sinkFile)
+import qualified Network.HTTP.Conduit as H
+import qualified Data.Conduit as C
+
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.UTF8 as UL
+
+import Data.Maybe
+
+
+-- Exploitation Now
+nextPage :: (ArrowXml a) => a XmlTree String
+nextPage = hasName "a" >>> hasAttrValue "class" (isInfixOf "navi-next") >>> hasAttr "href" >>> getAttrValue "href"
+
+comic :: (ArrowXml a) => a XmlTree String
+comic = hasAttrValue "id" (== "comic") >>> hasName "div" //> hasName "img" >>> hasAttr "src" >>> getAttrValue "src"
 
 
 main = do
     html <- readFile "test.html"
     let doc = readString [withParseHTML yes, withWarnings no] html
-    let nextPage = doc //> hasName "a" >>> hasAttrValue "class" (isInfixOf "navi-next") >>> hasAttr "href"
-
-    let comic = doc //> hasAttrValue "id" (== "comic") >>> hasName "div" //> hasName "img" >>> hasAttr "src"
 
     -- Image
-    img <- runX $ comic >>> getAttrValue "src"
+    let img = runLA (hread //> comic) html
+--    img <- runX $ doc //> comic
     mapM_ putStrLn img
 
+
+
     -- Navi
-    next <- runX $ nextPage >>> getAttrValue "href"
+    next <- runX $ doc //> nextPage
     mapM_ putStrLn next
+
+    -- List of next page let's try fetching em and reading em in.
+    nextStream <- fetch (map Webpage next)
+    let doc = map (readString [withParseHTML yes, withWarnings no] . UL.toString) (catMaybes nextStream)
+    next <- mapM (\a -> runX $ a //> nextPage) doc
+    mapM_ putStrLn (concat next)
+
+
+    putStrLn "hi"
+
+
+
+-- Data type of the url and any additional info needed
+data FetchType  = Webpage String
+                | Comic String String -- Url & Filename
+
+fetch :: [FetchType] -> IO [Maybe L.ByteString]
+fetch = withSocketsDo . mapM fetcher
+
+fetcher :: FetchType -> IO (Maybe L.ByteString)
+fetcher (Webpage u) = do
+    page <- H.simpleHttp u
+    return $ Just page
+fetcher (Comic u f) = do
+    page <- H.simpleHttp u
+    return $ Nothing
+
+{-
+    Simple version:
+        List of input url
+        emits into an conduit which then does http fetch stuff
+        then that either dump the file to disk or send it to hxt for parsing
+-}
