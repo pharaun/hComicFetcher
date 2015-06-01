e7c57bf62fcabb1194c614d6b11202096f87917b
Anja Berens <pharaun666@gmail.com>
2015-06-01 00:16:25 -0700

    Got the MonadIO version of runWebFetchT to work and it should now support any monad stack

--- src/Sites.hs
+++ src/Sites.hs
@@ -3,39 +3,6 @@ import Control.Monad
 import Control.Monad.Operational
 import Control.Monad.IO.Class
 
--- Tags
-data CTag = Initial -- The initial page
-          | Page Integer
-          deriving (Show)
-
-data ReplyType a = WebpageReply String a
-
-instance (Show a) => Show (ReplyType a) where
-    show (WebpageReply c t) = "WebpageReply: " ++ c ++ " - " ++ show t
-
-data FetchType a = Webpage String a
-                 | Image String
-
-instance (Show a) => Show (FetchType a) where
-    show (Webpage c t) = "Webpage: " ++ c ++ " - " ++ show t
-    show (Image c) = "Image: " ++ c
-
-
---
--- Callbacky solution with wrapping up the needed metadata as tags
---
-callback :: ReplyType CTag -> IO [FetchType CTag]
-callback (WebpageReply _ Initial) = do
-    return [Webpage "url1" (Page 1), Webpage "url2" (Page 2), Webpage "url3" (Page 3)]
-
-callback (WebpageReply _ (Page pg)) =
-    case pg of
-        1   -> return [Image "img1"]
-        2   -> return [Image "img2"]
-        3   -> return [Image "img3"]
-        _   -> return [Image "img_"]
-
-
 
 data ReplyType' = WebpageReply' String
     deriving (Show, Read)
@@ -56,82 +23,49 @@ fetchImage x = print $ case x of
                     "urlc"  -> "contentc"
                     _       -> "Unknown"
 
--- DFS
-s1 = do
-        _ <- fetchWebpage "idx1" (\idx -> do
-
-            -- Parsing
-            print idx
-
-            forM ["url1", "url2", "url3"] $ flip fetchWebpage (\chp -> do
-
-                -- do parsing stuff
-                print chp
-
-                forM ["urla", "urlb", "urlc"] fetchImage
-                )
-            )
-        return ()
-
--- BFS
-s2 = do
-    chpUrls <- fetchWebpage "idx1" (\idx -> do
-            -- Parsing
-            print idx
-            return ["url1", "url2", "url3"])
-
-    imgUrls <- forM chpUrls $ flip fetchWebpage (\chp -> do
-            -- Parsing
-            print chp
-            return ["urla", "urlb", "urlc"])
-
-    forM_ (concat imgUrls) fetchImage
-
-    return ()
-
 
 -- Reinversion/Operational
-data WebFetch a where
-    FetchWebpage :: [String] -> WebFetch String
-    FetchImage :: String -> WebFetch ()
-    Debug :: (Show s) => s -> WebFetch ()
+data WebFetchI a where
+    FetchWebpage :: [String] -> WebFetchI String
+    FetchImage :: String -> WebFetchI ()
+    Debug :: (Show s) => s -> WebFetchI ()
 
-type WebFetchMonad a = Program WebFetch a
+type WebFetchT m a = ProgramT WebFetchI m a
 
-runWebFetchMonad :: WebFetchMonad a -> IO a
-runWebFetchMonad = eval . view
+runWebFetchT :: (MonadIO m, Monad m) => WebFetchT m () -> m ()
+runWebFetchT = eval <=< viewT
   where
-    eval :: ProgramView WebFetch a -> IO a
-    eval (Return x)                 = return x
+    eval :: (MonadIO m, Monad m) => ProgramViewT WebFetchI m () -> m ()
+    eval (Return _)                 = return ()
 
     eval (FetchWebpage us :>>= k)   =
-        forM us (\u -> do
-            b <- fetchWebpage u (return . show)
-            runWebFetchMonad (k b))
-        >>= \(x:_) -> return x
+        forM_ us (\u -> do
+            b <- liftIO $ fetchWebpage u (return . show)
+            runWebFetchT (k b))
 
     eval (FetchImage u :>>= k)      = do
-        fetchImage u
-        runWebFetchMonad (k ())
+        liftIO $ fetchImage u
+        runWebFetchT (k ())
 
     eval (Debug s :>>= k)           = do
-        print s
-        runWebFetchMonad (k ())
+        liftIO $ print s
+        runWebFetchT (k ())
 
 
-fwp :: [String] -> WebFetchMonad String
+fwp :: [String] -> WebFetchT m String
 fwp = singleton . FetchWebpage
 
-fi :: String -> WebFetchMonad ()
+fi :: String -> WebFetchT m ()
 fi = singleton . FetchImage
 
-debug :: (Show a) => a -> WebFetchMonad ()
+debug :: (Show a) => a -> WebFetchT m ()
 debug = singleton . Debug
 
 
+s3 :: WebFetchT IO ()
 s3 = do
     debug "Approach one"
-    debug ""
+    liftIO $ putStrLn ""
 
     idx <- fwp ["idx1"]
 
@@ -148,6 +82,7 @@ s3 = do
 
     return ()
 
+s4 :: (Monad m) => WebFetchT m ()
 s4 = do
     debug "Approach two"
     debug ""
@@ -165,3 +100,6 @@ s4 = do
         )
 
     return ()
+
+
+test = runWebFetchT s3 >> putStrLn "" >> runWebFetchT s4
